3f7fe07286d38f3e1d5f8450e5856cd609246293
VikaBilyk <124256530+Observersss@users.noreply.github.com>
2024-09-13 00:00:54 +0300

    configure blog-sub-repo-server fix

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import {pool} from "./config/db";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5013;
 const BASE_URL = '/api/auth';
 
 app.use(express.json());
@@ -20,7 +20,8 @@ async function startApp(){
         } else {
             console.log('Connected to the database');
         }
-    })
+    });
+
     app.listen(PORT,() => {
         console.log(`Server has been started on port:${PORT}`);
     });
